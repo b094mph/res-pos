f951e25d47ed02c709b6effd37fdc3e41e8d53f0
bobby <b094mph@gmail.com>
2013-04-16 19:42:53 -0400

    added second level caching - ehcache

--- res-pos/src/main/java/com/res/model/Address.java
+++ res-pos/src/main/java/com/res/model/Address.java
@@ -11,8 +11,12 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
 @Entity
 @Table(name="address", catalog="restaurant")
+@Cache(region="addresses", usage=CacheConcurrencyStrategy.READ_WRITE)
 public class Address implements Serializable{
 
 	private static final long serialVersionUID = 1L;
