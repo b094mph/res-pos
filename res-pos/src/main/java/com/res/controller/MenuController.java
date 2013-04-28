c373284053a82193f1b26939ab8e1a4c84819a34
bobby <b094mph@gmail.com>
2013-04-28 16:34:27 -0400

    sunday

--- res-pos/src/main/java/com/res/controller/MenuController.java
+++ res-pos/src/main/java/com/res/controller/MenuController.java
@@ -2,20 +2,15 @@ package com.res.controller;
 
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.res.model.FoodCategory;
-import com.res.model.Menu;
 import com.res.service.MenuService;
 
 @Controller
