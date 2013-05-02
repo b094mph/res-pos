d59d976b9ab88115a88ba9545350e4ac3d29274b
bobby <b094mph@gmail.com>
2013-05-01 20:42:24 -0400

    updated login functionality to include welcome and loginfail pages

--- res-pos/WebContent/jsp/body/welcome.jsp
+++ res-pos/WebContent/jsp/body/welcome.jsp
@@ -1,18 +1,16 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <!DOCTYPE html>
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<jsp:include page="../common/bootstrap.jsp"/>
-	<jsp:include page="../common/jquery.jsp"/>
-	<title>Insert title here</title>
 </head>
 <body>
-<div class="container">
 	<div class="row">
-		<p>Login Failed.</p>
+		<h1>Welcome <c:out value="${username}"/> </h1>
+		
+		<a href='<c:url value="/j_spring_security_logout" />'>Logout</a>
 	</div>
-</div>
 </body>
 </html>
--- res-pos/src/main/java/com/res/controller/LoginController.java
+++ res-pos/src/main/java/com/res/controller/LoginController.java
@@ -1,9 +1,13 @@
 package com.res.controller;
 
+import java.security.Principal;
+
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -19,10 +23,29 @@ public class LoginController {
 	@Autowired
 	private AgentService agentService;
 	
-	@RequestMapping(value = "/login")
-	public ModelAndView showLoginHome(){
+	@RequestMapping(value = "/login", method=RequestMethod.GET)
+	public ModelAndView login(){
 		logger.info("Logging in...");
 		return new ModelAndView("login", "command", new Agent());
+	}
+	
+	@RequestMapping(value= "/loginfail", method=RequestMethod.GET)
+	public String loginFail(ModelMap model){
+		logger.info("Login failed...");
+		model.addAttribute("error", "true");
+		return "loginfail";
+	}
+	
+	@RequestMapping(value="/welcome", method=RequestMethod.GET)
+	public ModelAndView welcome(Principal principal){
+		ModelAndView mav = new ModelAndView("welcome");
+		
+		String username = principal.getName();
+		logger.info("in welcome " + username);
+		
+		mav.addObject("username", username);
+		
+		return mav;
 	}
 
 }
